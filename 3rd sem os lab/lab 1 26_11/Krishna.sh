6adab537d90b34fbeee61c8d7c918134ba1083e7
krishna050702 <75976947+krishna050702@users.noreply.github.com>
2021-11-28 07:27:31 +0530

    Add files via upload

---
+++ 3rd sem os lab/lab 1 26_11/Krishna.sh
@@ -0,0 +1,25 @@
+echo Name:- Krishna Mundada
+echo Roll no:- 45
+echo Section:- E3
+echo Subject:- Operating System lab 
+echo Branch:- AI-ML
+echo pwd command
+pwd
+echo ls command to show files list
+ls
+echo Calendar
+cal
+echo Date
+date
+echo Enter file content
+cat >File1.txt
+cat >>File1.txt
+echo Appended_file_
+cat File1.txt
+ls
+echo Lets copy test1.txt contents into test2.txt
+touch File2.txt
+cp File1.txt File2.txt
+echo Copied Succefully
+cat text2.txt
+
